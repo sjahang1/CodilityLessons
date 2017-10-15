1db370e6f762419b9308da5dcb7b32f1f235ce0d
Yash <Yash@192.168.0.132>
2017-10-16 05:54:35 +0800

    Simple change

--- codility/src/main/java/codility/Main.java
+++ codility/src/main/java/codility/Main.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		System.out.println("Hellow World");
-		System.out.println("**********************");
+		System.out.println("------------------");
 	}
 
 }
